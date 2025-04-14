c382ee790b14a6a90517c3264029d6fd00854c51
Nkosikenny <kennybhekumuzi@gmail.com>
2025-04-14 12:57:19 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
